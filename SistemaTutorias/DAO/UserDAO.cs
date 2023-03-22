285dc87c681aafee1f17d78f7bcf5404b00a2ce8
MIDGUET ARTURO GARCIA TORRES <zS18012193@estudiantes.uv.mx>
2023-03-22 01:13:02 -0600

    Agregar archivos de proyecto.

---
+++ SistemaTutorias/DAO/UserDAO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaTutorias.Domain;
+
+namespace SistemaTutorias.DAO
+{
+    internal class UserDAO
+    {
+        string connectionString = "Server=MB1,1433;Database=SistemaTutorias;User Id=SistemaTutorias;Password=Magt22081998;";
+        public UserDAO() {
+
+
+            
+             
+            
+        
+        }
+
+        public User prueba()
+        {
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                SqlCommand command = new SqlCommand("Select * from [dbo].[User]", connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) { 
+                    
+                    while (reader.Read())
+                    {
+                        Debug.WriteLine(reader.GetString(0) + reader.GetString(1));
+
+                    }
+                    
+                }
+                else
+                {
+                    Debug.WriteLine("Sin filas");
+                }
+
+                reader.Close();
+
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine(error.Message);
+                throw;
+            }
+            return new User();
+        }
+    }
+}
---
+++ SistemaTutorias/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SistemaTutorias.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SistemaTutorias
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            UserDAO user = new UserDAO();
+
+            user.prueba();
+        }
+    }
+}
