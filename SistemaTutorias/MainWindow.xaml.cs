9e87dd139e0c5ccc63f0ddc3dcfd38fdfd8d0a55
MIDGUET ARTURO GARCIA TORRES <zS18012193@estudiantes.uv.mx>
2023-03-22 02:44:05 -0600

    switch

--- SistemaTutorias/MainWindow.xaml.cs
+++ SistemaTutorias/MainWindow.xaml.cs
@@ -47,7 +47,16 @@ namespace SistemaTutorias
 
             if (password == user.password) {
 
-                Debug.WriteLine(user.usertype.ToString());
+                switch (user.usertype) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
                 
                 
             } else
