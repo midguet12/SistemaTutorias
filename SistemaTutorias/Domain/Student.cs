a98ad7064cf6a398491ce4e1fc5efe5641c83bfc
MIDGUET ARTURO GARCIA TORRES <zS18012193@estudiantes.uv.mx>
2023-03-29 11:51:16 -0600

    cambio en dao y clase estudiante.

--- SistemaTutorias/Domain/Student.cs
+++ SistemaTutorias/Domain/Student.cs
@@ -12,21 +12,24 @@ namespace SistemaTutorias.Domain
         public string nombre { get; set; }
         public string apellidoPaterno { get; set; }
         public string apellidoMaterno { get; set; }
-        public string periodo { get; set; }
         public string matricula { get; set; }
+        public string programaEducativo { get; set; }
+
+
 
         public Student()
         {
 
         }
 
-        public Student(string nombre, string apellidoPaterno, string apellidoMaterno, string periodo, string matricula)
+        public Student(string nombre, string apellidoPaterno, string apellidoMaterno, string programaEducativo, string matricula)
         {
             this.nombre = nombre;
             this.apellidoPaterno = apellidoPaterno;
             this.apellidoMaterno = apellidoMaterno;
-            this.periodo = periodo;
             this.matricula = matricula;
+            this.programaEducativo = programaEducativo;
+           
         }
     }
 
